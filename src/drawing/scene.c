6ca2864478fa66a1759571808bc1c5bdfaf2187a
Eason Chai <echai2905@gmail.com>
2023-01-31 00:16:22 +0800

    refactor to fit norm

--- src/drawing/scene.c
+++ src/drawing/scene.c
@@ -6,7 +6,7 @@
 /*   By: echai <echai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 16:48:56 by echai             #+#    #+#             */
-/*   Updated: 2023/01/31 00:09:31 by echai            ###   ########.fr       */
+/*   Updated: 2023/01/31 00:16:13 by echai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,24 @@ void	draw_texture(t_ctx *ctx, t_texture *texture, float deg)
 	}
 }
 
-// int	get_texture_offset()
-// {
-	
-// }
+void	get_texture_offset(t_ctx *ctx, t_texture *text, t_ray ray, float angle)
+{
+	text->y = text->y_offset * text->y_step;
+	if (ray.shade == 1)
+	{
+		text->x = (int)(ray.x) % 64;
+		if (rad_to_deg(angle) < 180)
+			text->x = 63 - text->x;
+	}
+	else
+	{
+		text->x = (int)(ray.y) % 64;
+		if (rad_to_deg(angle) < 90 || rad_to_deg(angle) > 270)
+			text->x = 63 - text->x;
+	}
+	text->color = get_texture(ctx, ray.side);
+	text->shade = ray.shade;
+}
 
 /**
  * @brief Draws the scene out
@@ -56,7 +70,7 @@ void	draw_scene(t_ctx *ctx, t_ray final_ray, float ray_angle, int deg)
 
 	ray_dist = final_ray.dist * cos(rad_limit(ctx->player->angle - ray_angle));
 	line_h = (64 * 320) / ray_dist;
-	texture.y_step = 64.0/line_h;
+	texture.y_step = 64.0 / line_h;
 	texture.y_offset = 0;
 	if (line_h > 320)
 	{
@@ -64,34 +78,8 @@ void	draw_scene(t_ctx *ctx, t_ray final_ray, float ray_angle, int deg)
 		line_h = 320;
 	}
 	line_o = 160 - line_h / 2;
-	texture.y = texture.y_offset * texture.y_step;
-	if (final_ray.shade == 1)
-	{
-		texture.x = (int)(final_ray.x) % 64;
-		if (rad_to_deg(ray_angle) < 180)
-			texture.x = 63 - texture.x;
-	}
-	else
-	{
-		texture.x = (int)(final_ray.y) % 64;
-		if (rad_to_deg(ray_angle) < 90 || rad_to_deg(ray_angle) > 270)
-			texture.x = 63 - texture.x;
-	}
-	texture.color = get_texture(ctx, final_ray.side);
-	texture.shade = final_ray.shade;
+	get_texture_offset(ctx, &texture, final_ray, ray_angle);
 	texture.line_h = line_h;
 	texture.line_o = line_o;
-	// int		y;
-	// int		x;
-	// int *color = get_texture(ctx, final_ray.side);
-	// y = -1;
-	// while (++y < line_h)
-	// {
-	// 	x = -1;
-	// 	while (++x < 8)
-	// 		put_pixel(ctx, (deg * 8 + 530) + x, (y + line_o),
-	// 			shade(color[(int)texture.x + ((int)texture.y * 64)], final_ray.shade));
-	// 	texture.y += texture.y_step;
-	// }
 	draw_texture(ctx, &texture, deg);
 }
