bb8c4799f2ed8e9aea64e9de6fed3248e7e807d0
maliew <maliew@student.42kl.edu.my>
2023-02-01 11:16:42 +0800

    Fix minimap sizing

--- src/minimap/minimap.c
+++ src/minimap/minimap.c
@@ -6,7 +6,7 @@
 /*   By: maliew <maliew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 15:21:40 by maliew            #+#    #+#             */
-/*   Updated: 2023/01/08 18:03:46 by maliew           ###   ########.fr       */
+/*   Updated: 2023/01/26 21:30:38 by maliew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	generate_map_image(t_ctx *ctx)
 	t_mlxx_img	*black;
 	t_mlxx_img	*white;
 
-	size = SCREEN_HEIGHT / 20;
+	size = 16;
 	black = mlxx_new_rect(ctx, size, size, 0x00111111);
 	white = mlxx_new_rect(ctx, size, size, 0x00FFFFFF);
 	ctx->map_image = mlxx_new_img(ctx->mlx, ctx->map_width * size,
@@ -59,8 +59,8 @@ int	generate_minimap_image(t_ctx *ctx)
 	if (ctx->minimap_image == NULL)
 		return (1);
 	mlxx_copy_image(ctx->minimap_image, ctx->map_image,
-		SCREEN_HEIGHT / 8 - ctx->player->x / 64 * SCREEN_HEIGHT / 20,
-		SCREEN_HEIGHT / 8 - ctx->player->y / 64 * SCREEN_HEIGHT / 20);
+		SCREEN_HEIGHT / 8 - ctx->player->x / 64 * 16,
+		SCREEN_HEIGHT / 8 - ctx->player->y / 64 * 16);
 	square = mlxx_new_rect(ctx, 3, 3, 0x00FF0000);
 	mlxx_copy_image(ctx->minimap_image, square, SCREEN_HEIGHT / 8 - 1,
 		SCREEN_HEIGHT / 8 - 1);
