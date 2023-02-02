d1b1907f7a00a6d3311659950eaf3d5053c37fdd
MTLKS <matthew.liew99@gmail.com>
2023-02-02 23:05:38 +0800

    norm

--- src/drawing/background.c
+++ src/drawing/background.c
@@ -6,7 +6,7 @@
 /*   By: maliew <maliew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 22:45:21 by maliew            #+#    #+#             */
-/*   Updated: 2023/02/02 22:51:34 by maliew           ###   ########.fr       */
+/*   Updated: 2023/02/02 23:05:09 by maliew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	set_background_color(t_ctx *ctx)
 {
 	t_mlxx_img	*buffer;
-	
+
 	buffer = mlxx_new_rect(ctx, SCREEN_WIDTH, SCREEN_HEIGHT / 2, ctx->ceiling);
 	mlxx_copy_image(ctx->img, buffer, 0, 0);
 	buffer = mlxx_new_rect(ctx, SCREEN_WIDTH, SCREEN_HEIGHT / 2, ctx->floor);
